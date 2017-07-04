27a790d6dea4d1e25d74af0cdd9a19c6f4ca94f5
Bradley Hilton <graywolf336@craftyn.com>
2017-07-04 00:47:32 -0500

    Add tslint to the project and build

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,18 @@ const gulp = require('gulp');
 const del = require('del');
 const sourcemaps = require('gulp-sourcemaps');
 const tsc = require('gulp-typescript');
+const tslint = require('gulp-tslint');
 const shell = require('gulp-shell');
-const spawn = require('child_process').spawn;
 
 const tsp = tsc.createProject('tsconfig.json');
 
 gulp.task('clean-generated', function _cleanTypescript() {
     const distFiles = ['./dist/**/*.*'];
     return del(distFiles);
+});
+
+gulp.task('lint-ts', function _lintTypescript() {
+    return tsp.src().pipe(tslint({ formatter: 'verbose' })).pipe(tslint.report());
 });
 
 gulp.task('compile-ts', ['clean-generated'], function _compileTypescript() {
@@ -19,35 +23,15 @@ gulp.task('compile-ts', ['clean-generated'], function _compileTypescript() {
             .pipe(gulp.dest('dist'));
 });
 
-let server;
-gulp.task('run-server', ['compile-ts'], function _runTheServer() {
-    if (server) server.kill();
-
-    server = spawn('node', ['server.js'], { stdio: 'inherit' });
-    server.on('close', (code) => {
-        if (code === 8) {
-            gulp.log('Error detected, waiting for changes....');
-        }
-    });
-});
-
-process.on('exit', () => {
-    if (server) server.kill();
-});
-
-gulp.task('defult', ['compile-ts', 'run-server'], function _watchCodeAndRun() {
-    gulp.watch('src/**/*.ts', ['compile-ts', 'run-server']);
-});
-
 //Tasks for getting it ready and publishing
 gulp.task('npm-files', function _npmFileGathering() {
     return gulp.src(['README.md', 'LICENSE', 'package.json']).pipe(gulp.dest('dist'));
 });
 
-gulp.task('pack', ['clean-generated', 'compile-ts', 'npm-files'], shell.task([
+gulp.task('pack', ['clean-generated', 'lint-ts', 'compile-ts', 'npm-files'], shell.task([
     'cd dist && npm pack'
 ]));
 
-gulp.task('publish', ['clean-generated', 'compile-ts', 'npm-files'], shell.task([
+gulp.task('publish', ['clean-generated', 'lint-ts', 'compile-ts', 'npm-files'], shell.task([
     'cd dist && npm publish && npm pack'
 ]));
