2649a562f94c8e521212d76b32251f6eef400523
Bradley Hilton <graywolf336@craftyn.com>
2017-07-04 02:31:50 -0500

    Version bump and rename addRocketlet to add

--- src/manager.ts
+++ src/manager.ts
@@ -1,6 +1,7 @@
 import { RocketletCompiler } from './compiler';
 import { IGetRocketletsFilter } from './interfaces';
 
+import * as AdmZip from 'adm-zip';
 import * as fs from 'fs';
 import * as path from 'path';
 import { Rocketlet } from 'temporary-rocketlets-ts-definition/Rocketlet';
@@ -12,11 +13,11 @@ export class RocketletManager {
     private readonly inactiveRocketlets: Map<number, Rocketlet>;
     private readonly compiler: RocketletCompiler;
 
-    constructor(public folder: string) {
+    constructor() {
         this.activeRocketlets = new Map<number, Rocketlet>();
         this.inactiveRocketlets = new Map<number, Rocketlet>();
         this.compiler = new RocketletCompiler();
-        console.log('Constructed the RocketletLoader and the src folder is:', this.folder);
+        console.log('Constructed the RocketletManager.');
     }
 
     public getCompiler(): RocketletCompiler {
@@ -24,38 +25,7 @@ export class RocketletManager {
     }
 
     public load(): Promise<Array<Rocketlet>> {
-        return new Promise((resolve, reject) => {
-            if (this.folder === '') {
-                return reject(new Error('Invalid source folder for loading Rocketlets from.'));
-            }
-
-            try {
-                fs.readdirSync(this.folder).forEach((file) => {
-                    const filePath = path.join(this.folder, file);
-                    // Verify it's a typescript file
-                    if (!file.endsWith('.ts')) {
-                        return;
-                    }
-
-                    // Verify it is actually a file
-                    const stat = fs.statSync(filePath);
-                    if (stat.isDirectory()) {
-                        return;
-                    }
-
-                    const src = this.getCompiler().toJs(fs.readFileSync(filePath, 'utf8'));
-                    console.log('toJs result:', src);
-                    // const rocketlet = this.getCompiler().toSandBox(src);
-
-                    // console.log(`Successfully loaded ${rocketlet.getName()}!`);
-                });
-            } catch (e) {
-                return reject(e);
-            }
-
-            // TODO: Combine the two maps values
-            return new Array<Rocketlet>();
-        });
+        throw new Error('Not implemented yet.');
     }
 
     public get(filter: IGetRocketletsFilter): Array<Rocketlet> {
@@ -70,13 +40,15 @@ export class RocketletManager {
         throw new Error('Not implemented yet.');
     }
 
-    // TODO: Determine what to do with this - add or addRocketlet?
-    public addRocketlet(source: string): Rocketlet {
-        throw new Error('Not implemented nor architected.');
+    public add(zipContentsBase64d: string): Rocketlet {
+        const zip = new AdmZip(new Buffer(zipContentsBase64d, 'base64'));
+
+        console.log(zip.getEntries());
+
+        throw new Error('Not implemented yet.');
     }
 
-    // TODO: Determine what to do with this - add or addRocketlet?
-    public removeRocketlet(id: number): Rocketlet {
+    public remove(id: number): Rocketlet {
         throw new Error('Not implemented nor architected.');
     }
 }
