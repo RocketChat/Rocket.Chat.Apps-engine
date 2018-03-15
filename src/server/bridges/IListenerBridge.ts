68712330470ee5cc70aad13ced0024d3258b55ae
Bradley Hilton <graywolf336@craftyn.com>
2018-03-15 13:40:49 -0300

    Fix the incorrect return value for roomEvent listener

--- src/server/bridges/IListenerBridge.ts
+++ src/server/bridges/IListenerBridge.ts
@@ -5,5 +5,5 @@ import { IRoom } from '@rocket.chat/apps-ts-definition/rooms';
 
 export interface IListenerBridge {
     messageEvent(int: AppInterface, message: IMessage): void | boolean | IMessage;
-    roomEvent(int: AppInterface, room: IRoom): void | boolean | IMessage;
+    roomEvent(int: AppInterface, room: IRoom): void | boolean | IRoom;
 }
