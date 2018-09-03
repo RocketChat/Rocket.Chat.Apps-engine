74edb3453f043ad9281d2c1b54c9aeebdfa51ed5
cardoso <matheus@cardo.so>
2018-09-03 19:45:30 -0300

    Add tests for setGroupable and getGroupable

--- tests/server/accessors/MessageBuilder.spec.ts
+++ tests/server/accessors/MessageBuilder.spec.ts
@@ -83,5 +83,13 @@ export class MessageBuilderAccessorTestFixture {
         Expect(mb.getMessage()).toBe(msg);
         delete msg.room;
         Expect(() => mb.getMessage()).toThrowError(Error, 'The "room" property is required.');
+
+        Expect(mb.setGroupable(true)).toBe(mb);
+        Expect(msg.groupable).toEqual(true);
+        Expect(mb.getGroupable()).toEqual(true);
+
+        Expect(mb.setGroupable(false)).toBe(mb);
+        Expect(msg.groupable).toEqual(false);
+        Expect(mb.getGroupable()).toEqual(false);
     }
 }
