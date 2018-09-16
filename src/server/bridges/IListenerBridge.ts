49c967d6b1487722c4f9edb22f1e5eaef620007d
Douglas Gubert <douglas.gubert@gmail.com>
2018-09-16 19:08:23 -0300

    Update types on IListenerBridge to include IDesktopNotification

--- src/server/bridges/IListenerBridge.ts
+++ src/server/bridges/IListenerBridge.ts
@@ -1,11 +1,11 @@
 import { AppInterface } from '../compiler';
 
 import { IMessage } from '../../definition/messages';
-import { INotification } from '../../definition/notifications';
+import { IDesktopNotification, INotification } from '../../definition/notifications';
 import { IRoom } from '../../definition/rooms';
 
 export interface IListenerBridge {
     messageEvent(int: AppInterface, message: IMessage): Promise<void | boolean | IMessage>;
     roomEvent(int: AppInterface, room: IRoom): Promise<void | boolean | IRoom>;
-    notificationEvent(int: AppInterface, notification: INotification): Promise<void | boolean | INotification>;
+    notificationEvent(int: AppInterface, notification: INotification | IDesktopNotification): Promise<void | boolean | INotification | IDesktopNotification>;
 }
