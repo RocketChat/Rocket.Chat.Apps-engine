d56182ed7cd115ec311c6d20871b2f60a1e048f0
Douglas Gubert <douglas.gubert@gmail.com>
2019-05-30 22:18:06 -0300

    Formatting improvement

--- src/server/accessors/LivechatUpdater.ts
+++ src/server/accessors/LivechatUpdater.ts
@@ -4,9 +4,11 @@ import { AppBridges } from '../bridges';
 
 export class LivechatUpdater implements ILivechatUpdater {
     constructor(private readonly bridges: AppBridges, private readonly appId: string) { }
+
     public transferVisitor(visitor: IVisitor, transferData: ILivechatTransferData): Promise<boolean> {
         return this.bridges.getLivechatBridge().transferVisitor(visitor, transferData, this.appId);
     }
+
     public closeRoom(room: ILivechatRoom, comment: string): Promise<boolean> {
         return this.bridges.getLivechatBridge().closeRoom(room, comment, this.appId);
     }
