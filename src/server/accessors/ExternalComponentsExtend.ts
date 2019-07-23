be51ecad053cbc0e739f021e5db4f10e97374c93
lolimay <1404363070@qq.com>
2019-07-24 07:26:20 +0800

    implement the interface IExternalComponentsExtend

---
+++ src/server/accessors/ExternalComponentsExtend.ts
@@ -0,0 +1,12 @@
+import { AppExternalComponentManager } from '../managers/AppExternalComponentManager';
+
+import { IExternalComponentsExtend } from '../../definition/accessors';
+import { IExternalComponent } from '../../definition/externalComponent/IExternalComponent';
+
+export class ExternalComponentsExtend implements IExternalComponentsExtend {
+    constructor(private readonly manager: AppExternalComponentManager, private readonly appId: string) { }
+
+    public async register(externalComponent: IExternalComponent): Promise<void> {
+        return Promise.resolve(this.manager.registerComponent(this.appId, externalComponent));
+    }
+}
