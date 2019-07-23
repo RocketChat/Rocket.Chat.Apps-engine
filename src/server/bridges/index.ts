24bc90a7adbf71331f79c4aeb4e1942114db150c
lolimay <1404363070@qq.com>
2019-07-24 07:30:41 +0800

    Implement IExternalComponentBridge

--- src/server/bridges/index.ts
+++ src/server/bridges/index.ts
@@ -4,6 +4,7 @@ import { IAppApiBridge } from './IAppApiBridge';
 import { IAppCommandBridge } from './IAppCommandBridge';
 import { IAppDetailChangesBridge } from './IAppDetailChangesBridge';
 import { IEnvironmentalVariableBridge } from './IEnvironmentalVariableBridge';
+import { IExternalComponentBridge } from './IExternalComponentBridge';
 import { IHttpBridge, IHttpBridgeRequestInfo } from './IHttpBridge';
 import { IInternalBridge } from './IInternalBridge';
 import { IListenerBridge } from './IListenerBridge';
@@ -15,6 +16,7 @@ import { IUserBridge } from './IUserBridge';
 
 export {
     IEnvironmentalVariableBridge,
+    IExternalComponentBridge,
     IHttpBridge,
     IHttpBridgeRequestInfo,
     IListenerBridge,
