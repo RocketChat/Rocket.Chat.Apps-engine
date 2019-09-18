83cb7f4a0b4ac10e5c5581ffea7639e4e1eeb8f4
lolimay <1404363070@qq.com>
2019-09-19 02:29:01 +0800

    feat: add unregisterComponent method
    We made follwing changes for the AppExternalComponentManager:
    * add new private property - appTouchedComponents
    * add new public method unregisterComponent
    * add new public method getExternalComponent

--- src/server/managers/AppExternalComponentManager.ts
+++ src/server/managers/AppExternalComponentManager.ts
@@ -6,23 +6,51 @@ import { IExternalComponent } from '../../definition/externalComponent';
  * An App will register external component during their `initialize` method.
  * Then once an App's `onEnable` is called and it returns true,
  * only then will that App's external component be enabled.
- *
- * Registered means the component has been provided to the bridged system.
  */
 export class AppExternalComponentManager {
     /**
      * The map that maintain the list of all registered components.
      */
     private providedComponents: Map<string, IExternalComponent>;
+    /**
+     * Contains the apps and the external components they have touhed.
+     * The key is appId and the value is external component.
+     * Doesn't matter whether the app provided, modified, disabled,
+     * or enabled. As long as an app touched an external component, then
+     * it is listed here.
+     */
+    private appTouchedComponents: Map<string, IExternalComponent>;
     constructor() {
         this.providedComponents = new Map<string, IExternalComponent>();
+        this.appTouchedComponents = new Map<string, IExternalComponent>();
     }
     /**
      * Add the external component to the providedComponents.
-     * @param appId the id of the app calling this
+     * @param appId the id of the app
      * @param externalComponent the external component to register
      */
     public registerComponent(appId: string, externalComponent: IExternalComponent): void {
         this.providedComponents.set(appId, externalComponent);
+        this.appTouchedComponents.set(appId, externalComponent);
+    }
+    /**
+     * Remove the external compoent from the providedComponents by appId.
+     * @param appId the id of the app
+     */
+    public unregisterComponent(appId: string): void {
+        if (this.providedComponents.has(appId)) {
+            this.providedComponents.delete(appId);
+        }
+    }
+    /**
+     * Get the external component by the appId.
+     * @param appId the id of the app
+     */
+    public getExternalComponent(appId: string): IExternalComponent {
+        if (this.appTouchedComponents.has(appId)) {
+            return this.appTouchedComponents.get(appId);
+        }
+
+        return null;
     }
 }
