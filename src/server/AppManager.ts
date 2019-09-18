b42b6ab8406bbeca79f2c3a7d0d95bf1998548e3
lolimay <1404363070@qq.com>
2019-09-19 02:43:48 +0800

    Add unregisterComponent in the lifecycle methods
    We need to call unregisterComponent method to remove the corresponding external  components from the providedComponents when following AppManager's methods are called:
    *  `unload` is called
    * `disable` is called <-> Disable an app
    *  `remove` is called <-> Uninstall an app
    *  `updateAppsMarketplaceInfo` is called and validateLicense not pass
    * `initializeApp` is called and validateLicense not pass
    * `enableApp` is called but is not success

--- src/server/AppManager.ts
+++ src/server/AppManager.ts
@@ -244,6 +244,7 @@ export class AppManager {
             if (rl.getStatus() === AppStatus.INITIALIZED) {
                 this.listenerManager.unregisterListeners(rl);
                 this.commandManager.unregisterCommands(rl.getID());
+                this.externalComponentManager.unregisterComponent(rl.getID());
                 this.apiManager.unregisterApis(rl.getID());
                 this.accessorManager.purifyApp(rl.getID());
                 continue;
@@ -359,6 +360,7 @@ export class AppManager {
 
         this.listenerManager.unregisterListeners(rl);
         this.commandManager.unregisterCommands(rl.getID());
+        this.externalComponentManager.unregisterComponent(rl.getID());
         this.apiManager.unregisterApis(rl.getID());
         this.accessorManager.purifyApp(rl.getID());
 
@@ -430,7 +432,6 @@ export class AppManager {
 
         return aff;
     }
-
     public async remove(id: string): Promise<ProxiedApp> {
         const app = this.apps.get(id);
 
@@ -440,6 +441,7 @@ export class AppManager {
 
         this.listenerManager.unregisterListeners(app);
         this.commandManager.unregisterCommands(app.getID());
+        this.externalComponentManager.unregisterComponent(app.getID());
         this.apiManager.unregisterApis(app.getID());
         this.accessorManager.purifyApp(app.getID());
         await this.bridges.getPersistenceBridge().purge(app.getID());
@@ -594,6 +596,7 @@ export class AppManager {
                 }
 
                 this.commandManager.unregisterCommands(app.getID());
+                this.externalComponentManager.unregisterComponent(app.getID());
                 this.apiManager.unregisterApis(app.getID());
 
                 return app.setStatus(AppStatus.INVALID_LICENSE_DISABLED);
@@ -686,6 +689,7 @@ export class AppManager {
 
             console.error(e);
             this.commandManager.unregisterCommands(storageItem.id);
+            this.externalComponentManager.unregisterComponent(storageItem.id);
             this.apiManager.unregisterApis(storageItem.id);
             result = false;
 
@@ -754,11 +758,15 @@ export class AppManager {
         }
 
         if (enable) {
+            const externalComponent = this.externalComponentManager.getExternalComponent(app.getID());
+
             this.commandManager.registerCommands(app.getID());
+            this.externalComponentManager.registerComponent(app.getID(), externalComponent);
             this.apiManager.registerApis(app.getID());
             this.listenerManager.registerListeners(app);
         } else {
             this.commandManager.unregisterCommands(app.getID());
+            this.externalComponentManager.unregisterComponent(app.getID());
             this.apiManager.unregisterApis(app.getID());
         }
 
