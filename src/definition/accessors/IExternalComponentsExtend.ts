29fe99d7ecb146d2c73780cb3784ca5c64707fe3
lolimay <lolimay@lolimay.cn>
2019-10-04 08:27:17 +0800

    Add documentation to register method

--- src/definition/accessors/IExternalComponentsExtend.ts
+++ src/definition/accessors/IExternalComponentsExtend.ts
@@ -5,5 +5,12 @@ import { IExternalComponent } from '../externalComponent';
  * component. Provided during the initialization of your App.
  */
 export interface IExternalComponentExtend {
+    /**
+     * Register an external component to the system. It actually calls
+     * addExternalComponent method of AppExternalComponentManager internally,
+     * adding the external component to appTouchedExternalComponents map.
+     *
+     * @param externalComponent the external component to be registered
+     */
     register(externalComponent: IExternalComponent): Promise<void>;
 }
