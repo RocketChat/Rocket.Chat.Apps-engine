c250945c0052215f62835d6c07400c59a556265e
lolimay <lolimay@lolimay.cn>
2019-10-08 22:48:03 +0800

    Remove ExternalComponentAlreadyTouchedError

--- src/server/errors/index.ts
+++ src/server/errors/index.ts
@@ -1,7 +1,6 @@
 import { CommandAlreadyExistsError } from './CommandAlreadyExistsError';
 import { CommandHasAlreadyBeenTouchedError } from './CommandHasAlreadyBeenTouchedError';
 import { CompilerError } from './CompilerError';
-import { ExternalComponentAlreadyTouchedError } from './ExternalComponentAlreadyTouchedError';
 import { ExternalComponentNotMatchWithAppError } from './ExternalComponentNotMatchWithAppError';
 import { InvalidLicenseError } from './InvalidLicenseError';
 import { MustContainFunctionError } from './MustContainFunctionError';
@@ -13,7 +12,6 @@ import { RequiredApiVersionError } from './RequiredApiVersionError';
 export {
     CommandAlreadyExistsError,
     CommandHasAlreadyBeenTouchedError,
-    ExternalComponentAlreadyTouchedError,
     ExternalComponentNotMatchWithAppError,
     PathAlreadyExistsError,
     CompilerError,
