8556115e793ae564ab57139653918e55f5660ba1
Bradley Hilton <graywolf336@craftyn.com>
2019-10-10 08:42:17 -0500

    Revert async iterable iterator for subscriptions

--- src/server/bridges/IRoomSubscriptionBridge.ts
+++ src/server/bridges/IRoomSubscriptionBridge.ts
@@ -1,7 +1,7 @@
 import { IRoomSubscription } from '../../definition/subscriptions';
 
 export interface IRoomSubscriptionBridge {
-    getByRoomId(roomId: string, appId: string): Promise<AsyncIterableIterator<IRoomSubscription>>;
+    getByRoomId(roomId: string, appId: string): Promise<IterableIterator<IRoomSubscription>>;
 
-    getByUserId(userId: string, appId: string): Promise<AsyncIterableIterator<IRoomSubscription>>;
+    getByUserId(userId: string, appId: string): Promise<IterableIterator<IRoomSubscription>>;
 }
