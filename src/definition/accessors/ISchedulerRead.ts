75382a3bdab536e0d9c33f6ac5f5fe6f020a5a9d
Bradley Hilton <graywolf336@craftyn.com>
2019-10-17 21:22:11 -0500

    Some a few bits for the scheduler

--- src/definition/accessors/ISchedulerRead.ts
+++ src/definition/accessors/ISchedulerRead.ts
@@ -1,6 +1,11 @@
 import { IJob } from '../scheduler';
+import { IUser } from '../users';
 
 export interface ISchedulerRead {
     /** Gets all of the jobs for this App. */
     getJobs(): Promise<Array<IJob>>;
+    /** Gets all of the jobs which have been created by the provided user for this App. */
+    getJobsByCreator(user: IUser): Promise<Array<IJob>>;
+    /** Gets a job by the provided id which this App created. */
+    getJobById(id: string): Promise<Array<IJob>>;
 }
--- src/definition/scheduler/IJob.ts
+++ src/definition/scheduler/IJob.ts
@@ -4,13 +4,29 @@ import { IJobResult } from './IJobResult';
 
 export interface IJob {
     /**
+     * The unique identifier for this job.
+     * No two jobs can have the same id. Should
+     * the same id be provided when scheduling a
+     * job and that id is already schedule, then
+     * an error will occur. So, please ensure
+     * the ids are unique. If you don't care about
+     * providing your own, leave it `undefined`
+     * and a random one will be provided.
+     */
+    id?: string;
+    /**
      * The name of the job. As this property is not unique,
      * we recommend setting this to something that is
      * user friendly since the list of job names will be visible
      * to the server administrator.
      */
     name: string;
-    /** A brief description of what this job is for. */
+    /**
+     * A brief description of what this job is for.
+     * We recommend setting this to something that is
+     * user friendly since the list of job names will be visible
+     * to the server administrator.
+     */
     description?: string;
     /** The user who created this job, but not always required. */
     creator?: IUser;
