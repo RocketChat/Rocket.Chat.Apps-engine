804975986f0e0de69654b4f0b90371a8c089366c
lolimay <lolimay@lolimay.cn>
2019-10-20 13:24:10 +0800

    export modules

--- src/definition/externalComponent/index.ts
+++ src/definition/externalComponent/index.ts
@@ -1,1 +1,9 @@
-export { IExternalComponent } from './IExternalComponent';
+import { IExternalComponent } from './IExternalComponent';
+import { IPostExternalComponentClosed } from './IPostExternalComponentClosed';
+import { IPostExternalComponentOpened } from './IPostExternalComponentOpened';
+
+export {
+    IExternalComponent,
+    IPostExternalComponentClosed,
+    IPostExternalComponentOpened,
+};
