5794d13ab29c1194659dcbf3ccd5cebe244a4bef
lolimay <lolimay@lolimay.cn>
2019-11-08 00:46:40 +0800

    try to pass circle ci

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,11 @@
 module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
     roots: ['<rootDir>/tests/server'],
     coverageDirectory: 'coverage',
     transform: {
         '^.+\\.ts?$': 'ts-jest',
     },
     moduleFileExtensions: ['ts', 'js', 'json', 'node'],
-    setupFilesAfterEnv: ['jest-extended']
+    setupFilesAfterEnv: ['jest-extended'],
 };
