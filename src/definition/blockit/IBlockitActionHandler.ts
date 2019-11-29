ee60dea4a506331e224c5f5ecabdae2a886a5099
Diego Sampaio <chinello@gmail.com>
2019-11-29 19:36:43 -0300

    View submit action handler

--- src/definition/blockit/IBlockitActionHandler.ts
+++ src/definition/blockit/IBlockitActionHandler.ts
@@ -2,16 +2,27 @@ import { IHttp, IModify, IPersistence, IRead } from '../accessors';
 import { AppMethod } from '../metadata';
 import { IBlockitAction } from './IBlockitAction';
 import { IBlockitResponse } from './IBlockitResponse';
+import { IBlockitViewSubmit } from './IBlockitViewSubmit';
 
 /** Handler for after a message is sent. */
 export interface IBlockitActionHandler {
     /**
-     * Method called *after* the message is sent to the other clients.
+     * Method called when a block action is invoked.
      *
      * @param data
      * @param read An accessor to the environment
      * @param http An accessor to the outside world
      * @param persistence An accessor to the App's persistence
      */
-    [AppMethod.BLOCKIT_BLOCK_ACTION](data: IBlockitAction, read: IRead, http: IHttp, persistence: IPersistence, modify: IModify): Promise<IBlockitResponse>;
+    [AppMethod.BLOCKIT_BLOCK_ACTION]?(data: IBlockitAction, read: IRead, http: IHttp, persistence: IPersistence, modify: IModify): Promise<IBlockitResponse>;
+
+    /**
+     * Method called when a modal is submitted.
+     *
+     * @param data
+     * @param read An accessor to the environment
+     * @param http An accessor to the outside world
+     * @param persistence An accessor to the App's persistence
+     */
+    [AppMethod.BLOCKIT_VIEW_SUBMIT]?(data: IBlockitViewSubmit, read: IRead, http: IHttp, persistence: IPersistence, modify: IModify): Promise<IBlockitResponse>;
 }
