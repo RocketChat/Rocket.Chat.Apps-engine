4203eb8a6932d04aa560957457a1a60a5f96e823
lolimay <lolimay@lolimay.cn>
2019-12-06 02:41:39 +0800

    Update user type

--- src/definition/users/UserType.ts
+++ src/definition/users/UserType.ts
@@ -1,4 +1,6 @@
 export enum UserType {
+    /** A user type for Rocket.Chat apps. */
+    APP,
     /** The user is a regular user of the system. */
     USER,
     /** A special user type for bots. */
