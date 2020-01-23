8641cc7e14bff6c3da381e299bbe5048a05fd66b
Bradley Hilton <graywolf336@craftyn.com>
2020-01-23 15:42:13 -0300

    WHoopies :see_no_evil: forgot to ensure it compiled

--- src/server/accessors/SchedulerRead.ts
+++ src/server/accessors/SchedulerRead.ts
@@ -1,11 +1,20 @@
 import { ISchedulerRead } from '../../definition/accessors';
 import { IJob } from '../../definition/scheduler';
+import { IUser } from '../../definition/users';
 
 export class SchedulerRead implements ISchedulerRead {
     constructor(private appId: string) { }
 
     public getJobs(): Promise<Array<IJob>> {
         console.log(`The app ${ this.appId } is getting all of their jobs`);
+        throw new Error('Method not implemented.');
+    }
+
+    public getJobsByCreator(user: IUser): Promise<Array<IJob>> {
+        throw new Error('Method not implemented.');
+    }
+
+    public getJobById(id: string): Promise<Array<IJob>> {
         throw new Error('Method not implemented.');
     }
 }
