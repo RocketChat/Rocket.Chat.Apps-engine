9d64046d27472804f6b1cd25d5f19e5f1da7b32d
jaclaf <jacqueline.lafloufa@gmail.com>
2020-02-06 10:36:22 -0300

    Update IMessageRead.ts
    Adding more info for a better documentation of this accessor.

--- src/definition/accessors/IMessageRead.ts
+++ src/definition/accessors/IMessageRead.ts
@@ -3,13 +3,30 @@ import { IRoom } from '../rooms/IRoom';
 import { IUser } from '../users/IUser';
 
 /**
- * This accessor provides methods for accessing
- * messages in a read-only-fashion.
+ * Accessor methods are used to fetch private data stored within an object. This accessor provides methods for accessing messages in a read-only fashion. These methods do not allow you to modify messages. 
+ * The methods below will recall a message, the room where a message was sent or the user that send a message if you provide the message's ID. 
+ * We recommend using this interface to follow cybersecurity guidelines (by storing only the message IDs, the accessor guarantees some level of anonymity of its contents) and for a better storage management (less stored data on the server).  
+ * 
+ * __What can go wrong?__
+ * If you are experiencing problems using these methods, it's wise to double check if the message ID you're using to gather the data really exists. 
+ * If the message ID exists but you're still experiencing problems, the error is probably related to infrastructure issues, such as connecting to databases or other network related issues.  
  */
 export interface IMessageRead {
-    getById(id: string): Promise<IMessage | undefined>;
 
+/**
+* Based on the ID provided, this method returns the content of a message. 
+*/
+    
+    getById(id: string): Promise<IMessage | undefined>;
+    
+/**
+* Based on the ID provided, this method returns the user that sent the message.  
+*/
     getSenderUser(messageId: string): Promise<IUser | undefined>;
 
+/**
+* Based on the ID provided, this method returns the room where the message was sent.
+*/   
+    
     getRoom(messageId: string): Promise<IRoom | undefined>;
 }
