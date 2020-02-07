c5caf72072e7c608816593e882c5e555c60c9e98
Douglas Gubert <douglas.gubert@gmail.com>
2020-02-07 17:44:54 -0300

    Add overflow menu element

--- src/definition/uikit/blocks/BlockBuilder.ts
+++ src/definition/uikit/blocks/BlockBuilder.ts
@@ -9,6 +9,7 @@ import {
     IInputElement,
     IInteractiveElement,
     IMultiStaticSelectElement,
+    IOverflowMenuElement,
     IPlainTextInputElement,
     ISelectElement,
     IStaticSelectElement,
@@ -27,6 +28,7 @@ type InputBlockParam = BlockFunctionParameter<IInputBlock>;
 
 type ButtonElementParam = ElementFunctionParameter<IButtonElement>;
 type ImageElementParam = ElementFunctionParameter<IImageElement>;
+type OverflowMenuElementParam = ElementFunctionParameter<IOverflowMenuElement>;
 type PlainTextInputElementParam = ElementFunctionParameter<IPlainTextInputElement>;
 type StaticSelectElementParam = ElementFunctionParameter<IStaticSelectElement>;
 type MultiStaticSelectElementParam = ElementFunctionParameter<IMultiStaticSelectElement>;
@@ -105,6 +107,13 @@ export class BlockBuilder {
             type: BlockElementType.IMAGE,
             ...info,
        };
+    }
+
+    public newOverflowMenuElement(info: OverflowMenuElementParam): IOverflowMenuElement {
+        return this.newInteractiveElement({
+            type: BlockElementType.OVERFLOW_MENU,
+            ...info,
+        } as IOverflowMenuElement);
     }
 
     public newPlainTextInputElement(info: PlainTextInputElementParam): IPlainTextInputElement {
--- src/definition/uikit/blocks/Objects.ts
+++ src/definition/uikit/blocks/Objects.ts
@@ -9,9 +9,14 @@ export interface ITextObject {
     emoji?: boolean;
 }
 
-export interface IConfirmationObject {
+export interface IConfirmationDialogObject {
     title: ITextObject;
     text: ITextObject;
     confirm: ITextObject;
     deny: ITextObject;
+}
+
+export interface IOptionObject {
+    text: ITextObject;
+    value: string;
 }
