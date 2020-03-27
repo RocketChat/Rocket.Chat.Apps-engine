16f55703d1e700d8aa0a3f1e619aee652eb61f39
lolimay <lolimay@lolimay.cn>
2020-03-27 13:49:00 +0800

    Fix linting

--- tests/server/managers/AppListenerManager.spec.ts
+++ tests/server/managers/AppListenerManager.spec.ts
@@ -27,7 +27,7 @@ export class AppListenerManagerTestFixture {
             },
             getOneById(appId: string) {
                 return this.mockApp;
-            }
+            },
         } as AppManager;
     }
 
