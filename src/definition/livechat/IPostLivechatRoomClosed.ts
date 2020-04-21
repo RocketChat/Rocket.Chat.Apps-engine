17b86cacf4541fc84dfffae138738748460e64a6
Shiqi Mei <shiqi.mei@rocket.chat>
2020-04-22 02:21:15 +0800

    Update src/definition/livechat/IPostLivechatRoomClosed.ts
    Co-Authored-By: Douglas Gubert <d-gubert@users.noreply.github.com>

--- src/definition/livechat/IPostLivechatRoomClosed.ts
+++ src/definition/livechat/IPostLivechatRoomClosed.ts
@@ -14,5 +14,5 @@ export interface IPostLivechatRoomClosed {
      * @param http An accessor to the outside world
      * @param persistence An accessor to the App's persistence
      */
-    [AppMethod.EXECUTE_POST_LIVECHAT_ROOM_CLOSED](data: ILivechatRoom, read: IRead, http: IHttp, persistence: IPersistence): Promise<void>;
+    [AppMethod.EXECUTE_POST_LIVECHAT_ROOM_CLOSED](room: ILivechatRoom, read: IRead, http: IHttp, persistence: IPersistence): Promise<void>;
 }
