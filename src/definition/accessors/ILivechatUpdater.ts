71494ecaee222741af86c1fc1c53993ba258190a
Shiqi Mei <shiqi.mei@rocket.chat>
2020-09-19 06:48:30 +0800

    [NEW] Provide new Livechat Api setCustomFields (#313)
    * Provide new Livechat Api setCustomFields
    
    * fix: overwrite is a required param
    
    * Doc setCustomFields' return type
    
    * Fix bridge return type
    
    * Introduce livechatData field to visitor and room
    
    Co-authored-by: Douglas Gubert <douglas.gubert@gmail.com>

--- src/definition/accessors/ILivechatUpdater.ts
+++ src/definition/accessors/ILivechatUpdater.ts
@@ -17,4 +17,15 @@ export interface ILivechatUpdater {
      * @param comment The comment explaining the reason for closing the room
      */
     closeRoom(room: IRoom, comment: string): Promise<boolean>;
+
+    /**
+     * Set a livechat visitor's custom fields by its token
+     * @param token The visitor's token
+     * @param key The key in the custom fields
+     * @param value The value to be set
+     * @param overwrite Whether overwrite or not
+     *
+     * @returns Promise to whether success or not
+     */
+    setCustomFields(token: IVisitor['token'], key: string, value: string, overwrite: boolean): Promise<boolean>;
 }
