8a8397a980d70cced6d23dc7025135c1d0741d5f
Douglas Gubert <douglas.gubert@gmail.com>
2020-10-23 19:04:40 -0300

    WIP processor wrapper

--- src/server/managers/AppSchedulerManager.ts
+++ src/server/managers/AppSchedulerManager.ts
@@ -5,16 +5,37 @@ import {
 } from '../../definition/scheduler';
 import { AppManager } from '../AppManager';
 import { IAppSchedulerBridge } from '../bridges/IAppSchedulerBridge';
+import { AppAccessorManager } from './';
+
+function createProcessorId(jobId, appId) {
+	return `${ jobId }_${ appId }`;
+}
 
 export class AppSchedulerManager {
     private readonly bridge: IAppSchedulerBridge;
+    private readonly accessors: AppAccessorManager;
+
+    private registeredProcessors: Map<string, {[processorId: string]: IProcessor}>;
 
     constructor(private readonly manager: AppManager) {
         this.bridge = this.manager.getBridges().getSchedulerBridge();
+        this.accessors = this.manager.getAccessorManager();
     }
 
     public registerProcessors(processors: Array<IProcessor> = [], appId: string): void {
-        this.bridge.registerProcessors(processors,  appId);
+        const processorId = createProcessorId(processor.id, appId);
+
+        if (!this.registeredProcessors.get(appId)) {
+            this.registeredProcessors.set(appId, {});
+        }
+
+        this.registeredProcessors.get(appId)[processorId] = processor;
+
+        this.bridge.registerProcessor({ ...processor, id: processorId },  appId);
+    }
+
+    public async callProcessor(processorId: string, data: object): Promise<void> {
+        
     }
 
     public async scheduleOnce(job: IOnetimeSchedule, appId: string): Promise<void> {
