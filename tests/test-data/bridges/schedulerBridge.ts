f3197bde37763cfc346ec5251ce360867dfb5e23
Douglas Gubert <douglas.gubert@gmail.com>
2020-10-24 18:21:19 -0300

    Fix tests

---
+++ tests/test-data/bridges/schedulerBridge.ts
@@ -0,0 +1,21 @@
+import { IOnetimeSchedule, IProcessor, IRecurringSchedule } from '../../../src/definition/scheduler';
+import { IAppSchedulerBridge } from '../../../src/server/bridges';
+
+export class TestSchedulerBridge implements IAppSchedulerBridge {
+    public registerProcessors(processor: Array<IProcessor>, appId: string): void {
+        throw new Error('Method not implemented.');
+    }
+    public async scheduleOnce(job: IOnetimeSchedule, appId: string): Promise<void> {
+        throw new Error('Method not implemented.');
+    }
+    public async scheduleRecurring(job: IRecurringSchedule, appId: string): Promise<void> {
+        throw new Error('Method not implemented.');
+    }
+    public async cancelJob(jobId: string, appId: string): Promise<void> {
+        throw new Error('Method not implemented.');
+    }
+    public async cancelAllJobs(appId: string): Promise<void> {
+        throw new Error('Method not implemented.');
+    }
+
+}
