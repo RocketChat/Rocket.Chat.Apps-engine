eb3b7642625b22be95cd687c6102db4fa8bd14bc
Thassio Victor <tvmcarvalho@gmail.com>
2020-11-04 20:31:36 -0300

    Allow jobs to be scheduled along with processor registration (#342)
    * Add documentation for the Scheduler API
    
    * Add support for `startupSetting` for executing jobs at app startup
    
    * Remove "TODO" comment
    
    * Make the call to `registerProcessors` sync
    
    * Make the documentation more clear
    
    * Adjust code based on suggestions
    
    * Document exported interfaces
    
    * Let `startupSetting` be `undefined` when not provided
    
    * Apply code review
    
    * Make `registerProcessors` async
    
    * Update method signature from test data
    
    * Pass data through the `startupSetting` from processors
    
    Co-authored-by: Douglas Gubert <d-gubert@users.noreply.github.com>

--- src/server/bridges/IAppSchedulerBridge.ts
+++ src/server/bridges/IAppSchedulerBridge.ts
@@ -4,10 +4,44 @@ import {
     IRecurringSchedule,
 } from '../../definition/scheduler';
 
+/**
+ * The interface which should be implemented for the apps to have
+ * access to the Job Scheduler
+ */
 export interface IAppSchedulerBridge {
-    registerProcessors(processor: Array<IProcessor>, appId: string): void;
+    /**
+     * Register processors that can be scheduled to run
+     *
+     * @param {Array<IProcessor>} processors An array of processors
+     * @param appId the id of the app calling this
+     */
+    registerProcessors(processor: Array<IProcessor>, appId: string): Promise<void>;
+
+    /**
+     * Schedules a registered processor to run _once_.
+     *
+     * @param {IOnetimeSchedule} job
+     * @param appId the id of the app calling this
+     */
     scheduleOnce(job: IOnetimeSchedule, appId: string): Promise<void>;
+    /**
+     * Schedules a registered processor to run in recurrencly acording to a given interval
+     *
+     * @param {IRecurringSchedule} job
+     * @param appId the id of the app calling this
+     */
     scheduleRecurring(job: IRecurringSchedule, appId: string): Promise<void>;
+    /**
+     * Cancels a running job given its jobId
+     *
+     * @param {string} jobId
+     * @param appId the id of the app calling this
+     */
     cancelJob(jobId: string, appId: string): Promise<void>;
+    /**
+     * Cancels all the running jobs from the app
+     *
+     * @param appId the id of the app calling this
+     */
     cancelAllJobs(appId: string): Promise<void>;
 }
