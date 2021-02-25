4875e4f23ce97f4a819732ea21e8c999646bb2aa
Douglas Gubert <douglas.gubert@gmail.com>
2021-02-25 12:52:01 -0300

    Improve comments on CloudWorkspace

--- src/server/managers/AppPermissionManager.ts
+++ src/server/managers/AppPermissionManager.ts
@@ -49,8 +49,8 @@ export class AppPermissionManager {
     /**
      * It returns the declaration of the permission if the app declared, or it returns `undefined`.
      */
-    public static hasPermission<P extends IPermission>(appId: string, permission: P): undefined | P {
-        const grantedPermission = getPermissionsByAppId(appId).find(({ name = '' }) => name === permission.name);
+    public static hasPermission<P extends IPermission>(appId: string, permission: P): P | undefined {
+        const grantedPermission = getPermissionsByAppId(appId).find(({ name }) => name === permission.name) as unknown;
 
         if (!grantedPermission) {
             return undefined;
