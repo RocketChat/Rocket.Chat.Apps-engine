a24f20e97c5e1d2542ff0c71fd9b67accd918159
Lucas Sartor Chauvin <lucassartorchauvin@gmail.com>
2021-04-16 00:33:53 -0300

    remove console.warn

--- src/server/AppManager.ts
+++ src/server/AppManager.ts
@@ -749,7 +749,7 @@ export class AppManager {
             const status = AppStatus.ERROR_DISABLED;
 
             if (e.name === 'NotEnoughMethodArgumentsError') {
-                console.warn('Please report the following error:');
+                app.getLogger().warn('Please report the following error:');
             }
 
             result = false;
