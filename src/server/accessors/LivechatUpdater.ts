graywolf336/temporary-rocketlets-server

Add possibility to set room closer to LivechatUpdater.closeRoom

src/server/accessors/LivechatUpdater.ts
@@ -1,5 +1,6 @@
 import { ILivechatUpdater } from '../../definition/accessors';
 import { ILivechatRoom, ILivechatTransferData, IVisitor } from '../../definition/livechat';
+import { IUser } from '../../definition/users';
 import { AppBridges } from '../bridges';
 
 export class LivechatUpdater implements ILivechatUpdater {
@@ -9,8 +10,8 @@ export class LivechatUpdater implements ILivechatUpdater {
         return this.bridges.getLivechatBridge().transferVisitor(visitor, transferData, this.appId);
     }
 
-    public closeRoom(room: ILivechatRoom, comment: string): Promise<boolean> {
-        return this.bridges.getLivechatBridge().closeRoom(room, comment, this.appId);
+    public closeRoom(room: ILivechatRoom, comment: string, closer?: IUser): Promise<boolean> {
+        return this.bridges.getLivechatBridge().closeRoom(room, comment, closer, this.appId);
     }
 
     public setCustomFields(token: IVisitor['token'], key: string, value: string, overwrite: boolean): Promise<boolean> {

RocketChat/Rocket.Chat.Apps-engine

src/server/accessors/LivechatUpdater.ts
@@ -1,5 +1,6 @@
 import { ILivechatUpdater } from '../../definition/accessors';
 import { ILivechatRoom, ILivechatTransferData, IVisitor } from '../../definition/livechat';
+import { IUser } from '../../definition/users';
 import { AppBridges } from '../bridges';
 
 export class LivechatUpdater implements ILivechatUpdater {
@@ -9,8 +10,8 @@ export class LivechatUpdater implements ILivechatUpdater {
         return this.bridges.getLivechatBridge().doTransferVisitor(visitor, transferData, this.appId);
     }
 
-    public closeRoom(room: ILivechatRoom, comment: string): Promise<boolean> {
-        return this.bridges.getLivechatBridge().doCloseRoom(room, comment, this.appId);
+    public closeRoom(room: ILivechatRoom, comment: string, closer?: IUser): Promise<boolean> {
+        return this.bridges.getLivechatBridge().doCloseRoom(room, comment, closer, this.appId);
     }
 
     public setCustomFields(token: IVisitor['token'], key: string, value: string, overwrite: boolean): Promise<boolean> {