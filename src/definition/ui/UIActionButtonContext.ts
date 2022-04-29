d1dba85ac9a001d9e2a82e58ab63056281a33187
Allan RIbeiro <35040806+AllanPazRibeiro@users.noreply.github.com>
2022-04-29 08:48:33 -0300

    [NEW] Adding interfaces to add action button on user dropdown menu (#501)

--- src/definition/ui/UIActionButtonContext.ts
+++ src/definition/ui/UIActionButtonContext.ts
@@ -2,4 +2,5 @@ export enum UIActionButtonContext {
     MESSAGE_ACTION = 'messageAction',
     ROOM_ACTION = 'roomAction',
     MESSAGE_BOX_ACTION = 'messageBoxAction',
+    USER_DROPDOWN_ACTION = 'userDropdownAction',
 }
