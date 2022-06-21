98c8f874acd21b48cf101be670c8101240129510
thassiov <tvmcarvalho@gmail.com>
2022-06-21 13:18:08 -0300

    Dispatch actions when static item is selected

--- src/definition/uikit/blocks/Elements.ts
+++ src/definition/uikit/blocks/Elements.ts
@@ -11,6 +11,7 @@ export enum BlockElementType {
 
 export enum InputElementDispatchAction {
     ON_CHARACTER_ENTERED = 'on_character_entered',
+    ON_ITEM_SELECTED = 'on_item_selected',
 }
 
 export interface IBlockElement {
