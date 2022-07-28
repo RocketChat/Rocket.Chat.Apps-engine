30d51b4beeef91b5fbcec4cbb323d54df9cabf0e
thassiov <tvmcarvalho@gmail.com>
2022-07-28 18:04:01 -0300

    wip

--- src/server/compiler/AppCompiler.ts
+++ src/server/compiler/AppCompiler.ts
@@ -1,88 +1,210 @@
-import * as path from 'path';
+import * as path from "path";
+import { VMScript } from "vm2";
 
-import { App } from '../../definition/App';
-import { AppMethod } from '../../definition/metadata';
-import { AppAccessors } from '../accessors';
-import { AppManager } from '../AppManager';
-import { MustContainFunctionError, MustExtendAppError } from '../errors';
-import { AppConsole } from '../logging';
-import { ProxiedApp } from '../ProxiedApp';
-import { getRuntime } from '../runtime';
-import { buildCustomRequire } from '../runtime/require';
-import { IAppStorageItem } from '../storage';
-import { IParseAppPackageResult } from './IParseAppPackageResult';
+import { App } from "../../definition/App";
+import { AppMethod } from "../../definition/metadata";
+import { AppAccessors } from "../accessors";
+import { AppManager } from "../AppManager";
+import { MustContainFunctionError, MustExtendAppError } from "../errors";
+import { AppConsole } from "../logging";
+import { ProxiedApp } from "../ProxiedApp";
+import { getRuntime } from "../runtime";
+import { buildCustomRequire } from "../runtime/require";
+import { IAppStorageItem } from "../storage";
+import { IParseAppPackageResult } from "./IParseAppPackageResult";
 
 export class AppCompiler {
-    public normalizeStorageFiles(files: { [key: string]: string }): { [key: string]: string } {
+    public normalizeStorageFiles(files: { [key: string]: string }): {
+        [key: string]: string;
+    } {
         const result: { [key: string]: string } = {};
 
         Object.entries(files).forEach(([name, content]) => {
-            result[name.replace(/\$/g, '.')] = content;
+            result[name.replace(/\$/g, ".")] = content;
         });
 
         return result;
     }
 
-    public toSandBox(manager: AppManager, storage: IAppStorageItem, { files }: IParseAppPackageResult): ProxiedApp {
-        if (typeof files[path.normalize(storage.info.classFile)] === 'undefined') {
-            throw new Error(`Invalid App package for "${ storage.info.name }". ` +
-                `Could not find the classFile (${ storage.info.classFile }) file.`);
+    public toSandBox(
+        manager: AppManager,
+        storage: IAppStorageItem,
+        { files }: IParseAppPackageResult
+    ): ProxiedApp {
+        console.log(
+            `-----------------------------[toSandBox]-----------------------------`
+        );
+        // console.log(`[toSandBox] storage`, storage.id);
+        if (
+            typeof files[path.normalize(storage.info.classFile)] === "undefined"
+        ) {
+            throw new Error(
+                `Invalid App package for "${storage.info.name}". ` +
+                    `Could not find the classFile (${storage.info.classFile}) file.`
+            );
         }
 
         const Runtime = getRuntime();
 
         const customRequire = buildCustomRequire(files, storage.info.id);
-        const result = Runtime.runCode(files[path.normalize(storage.info.classFile)], {
-            require: customRequire,
-        });
+        // const app2 = App.toString()
 
-        console.log(storage.info.classFile, result);
+        // console.log(`[toSandBox] app2`, app2)
 
-        if (typeof result !== 'function') {
-            // tslint:disable-next-line:max-line-length
-            throw new Error(`The App's main class for ${ storage.info.name } is not valid ("${ storage.info.classFile }").`);
-        }
+        // console.log(`[toSandBox] customRequire`, customRequire);
 
+        const appCode = files[path.normalize(storage.info.classFile)];
         const appAccessors = new AppAccessors(manager, storage.info.id);
         const logger = new AppConsole(AppMethod._CONSTRUCTOR);
-        const rl = Runtime.runCode('new App(info, rcLogger, appAccessors);', {
-            rcLogger: logger,
+        // const resultt = Runtime.runCode(appCode, { require: customRequire });
+        const dalemanito = new VMScript(appCode);
+
+        const uepa = Runtime.runCode(dalemanito.code, {
+            require: customRequire
+        });
+        const className = storage.info.classFile.split(".")[0];
+
+        // console.log(`[toSandBox] files[path.normalize(storage.info.classFile)]`, files[path.normalize(storage.info.classFile)])
+
+        // const result = Runtime.runCode(files[path.normalize(storage.info.classFile)], {
+        //     require: customRequire,
+
+        // });
+
+        // console.log(`[toSandBox] result`, result)
+
+        // console.log(`[toSandBox] classFile`, storage.info.classFile);
+        // console.log(`[toSandBox] className`, className);
+
+        // console.log(`[toSandBox] result[className] 1`, result[className])
+        // console.log(`[toSandBox] result[className] 2`, result[className].constructor.toString())
+        // console.log(`[toSandBox] result[className] 3`, result[className].constructor.constructor.toString())
+
+        // if (typeof resultt[className] !== 'function') {
+        //     // tslint:disable-next-line:max-line-length
+        //     throw new Error(`The App's main class for ${ storage.info.name } is not valid ("${ storage.info.classFile }").`);
+        // }
+
+        // console.log(`[toSandBox] code result[className]`, new result[className]())
+        // console.log(`[toSandBox] runcode params`, {
+        //     logger,
+        //     info: storage.info.name,
+        //     App: result[className].constructor.constructor,
+        //     accessors: appAccessors,
+        //     require: {
+        //         context: 'host',
+        //     },
+        // })
+        // let rl;
+        // try {
+        //     rl = Runtime.runCode(`new App(info, logger, accessors);`, {
+        //         id: className,
+        //         logger,
+        //         info: storage.info,
+        //         App: result[className].constructor.constructor,
+        //         accessors: appAccessors,
+        //         require: {
+        //             context: 'host',
+        //         },
+        //     }, { timeout: 1000, filename: `App_${ storage.info.nameSlug }.js` }, `[toSandBox] rl`);
+        //     console.log(`[toSandBox] rl UEEEEEEEEEEEPA`, rl)
+        // } catch (error) {
+        //     console.error(`[toSandBox] `, error)
+        // }
+        // console.log(`[toSandBox] resultt`, resultt[className].toString());
+
+        // const appString = uepa[className]
+        //     .toString()
+        //     .replace("class", `class ${className}`);
+
+        // const app3 = (App.toString().concat('\n const y = {App}\n').concat(appString));
+        // console.log(`[toSandBox] resultt string`, app3);
+
+        // const parsedApp = new Function(appString);
+
+        // console.log(`[toSandBox] resultt parsed`, parsedApp.toString());
+        console.log(`[toSandBox] CLASS PARSED`, uepa[className].toString());
+
+        const newAppScript = `
+        console.log({info, logger, accessors});
+        const app = new App(info, logger, accessors);
+        console.log('NEW APP INSIDE SANDBOX =>>>', app);
+        console.log('INSIDE SANDBOX =>', app.getVersion());
+        module.exports = app;
+        `;
+
+        const rl = Runtime.runCode(newAppScript, {
+            id: className,
+            logger,
             info: storage.info,
-            App: result,
-            appAccessors,
-        }, { timeout: 1000, filename: `App_${ storage.info.nameSlug }.js` });
+            App: uepa[className],
+            accessors: appAccessors,
+            require: {
+                external: true,
+                context: "host",
+            },
+        });
 
-        if (!(rl instanceof App)) {
-            throw new MustExtendAppError();
-        }
+        console.log(
+            `--------------------------rl------------------------------`
+        );
+        // console.log(`[toSandBox] rl`, rl);
+        // console.log(`[toSandBox] rl string`, rl.toString());
+        // console.log(`[toSandBox] rl function`, new Function(rl.toString()));
 
-        if (typeof rl.getName !== 'function') {
-            throw new MustContainFunctionError(storage.info.classFile, 'getName');
-        }
+        // if (!(rl instanceof App)) {
+        //     throw new MustExtendAppError();
+        // }
 
-        if (typeof rl.getNameSlug !== 'function') {
-            throw new MustContainFunctionError(storage.info.classFile, 'getNameSlug');
-        }
+        // if ((typeof rl as any).getName !== "function") {
+        //     throw new MustContainFunctionError(
+        //         storage.info.classFile,
+        //         "getName"
+        //     );
+        // }
 
-        if (typeof rl.getVersion !== 'function') {
-            throw new MustContainFunctionError(storage.info.classFile, 'getVersion');
-        }
+        // if ((typeof rl as any).getNameSlug !== "function") {
+        //     throw new MustContainFunctionError(
+        //         storage.info.classFile,
+        //         "getNameSlug"
+        //     );
+        // }
 
-        if (typeof rl.getID !== 'function') {
-            throw new MustContainFunctionError(storage.info.classFile, 'getID');
-        }
+        // if ((typeof rl as any).getVersion !== "function") {
+        //     throw new MustContainFunctionError(
+        //         storage.info.classFile,
+        //         "getVersion"
+        //     );
+        // }
 
-        if (typeof rl.getDescription !== 'function') {
-            throw new MustContainFunctionError(storage.info.classFile, 'getDescription');
-        }
+        // if ((typeof rl as any).getID !== "function") {
+        //     throw new MustContainFunctionError(storage.info.classFile, "getID");
+        // }
 
-        if (typeof rl.getRequiredApiVersion !== 'function') {
-            throw new MustContainFunctionError(storage.info.classFile, 'getRequiredApiVersion');
-        }
+        // if ((typeof rl as any).getDescription !== "function") {
+        //     throw new MustContainFunctionError(
+        //         storage.info.classFile,
+        //         "getDescription"
+        //     );
+        // }
 
-        const app = new ProxiedApp(manager, storage, rl as App, new Runtime(rl as App, customRequire));
+        // if ((typeof rl as any).getRequiredApiVersion !== "function") {
+        //     throw new MustContainFunctionError(
+        //         storage.info.classFile,
+        //         "getRequiredApiVersion"
+        //     );
+        // }
 
-        manager.getLogStorage().storeEntries(app.getID(), logger);
+        const app = new ProxiedApp(
+            manager,
+            storage,
+            rl as unknown as App,
+            new Runtime(rl as unknown as App, customRequire) as any
+        );
+        // console.log(`[toSandBox] app`, app)
+        // manager.getLogStorage().storeEntries(app.getID(), logger);
+        // console.log(`[toSandBox] app`, app)
+        // manager.getLogStorage().storeEntries(parsedApp.getID(), logger);
 
         return app;
     }
--- src/server/runtime/AppsEngineVM2Runtime.ts
+++ src/server/runtime/AppsEngineVM2Runtime.ts
@@ -1,51 +1,125 @@
-import * as timers from 'timers';
-import { NodeVM, NodeVMOptions } from 'vm2';
-import { App } from '../../definition/App';
-import { APPS_ENGINE_RUNTIME_DEFAULT_TIMEOUT, AppsEngineRuntime, getFilenameForApp, IAppsEngineRuntimeOptions } from './AppsEngineRuntime';
+import { NodeVM, NodeVMOptions } from "vm2";
+import { App } from "../../definition/App";
+import { AppStatus } from "../../definition/AppStatus";
+import {
+    AppsEngineRuntime,
+    getFilenameForApp,
+    IAppsEngineRuntimeOptions,
+} from "./AppsEngineRuntime";
 
 export class AppsEngineVM2Runtime extends AppsEngineRuntime {
     public static defaultNodeVMOptions: NodeVMOptions = {
-        console: 'inherit',
+        // console: 'inherit',
         // wrapper: 'none',
-        timeout: APPS_ENGINE_RUNTIME_DEFAULT_TIMEOUT,
+        // timeout: APPS_ENGINE_RUNTIME_DEFAULT_TIMEOUT,
         // we don't need any compiling happening
         compiler: (code: string, filename: string) => code,
         // We keep require inaccessible here as we expect it to be provided
         // require: false,
-        sandbox: {
-            Buffer,
-            ...timers,
-        },
+        // sandbox: {
+        //     Buffer,
+        //     ...timers,
+        // },
     };
 
-    public static runCode(code: string, sandbox?: Record<string, any>, options?: IAppsEngineRuntimeOptions): any {
+    public static runCode(
+        code: string,
+        sandbox?: Record<string, any>,
+        options?: IAppsEngineRuntimeOptions
+    ): any {
+
+        const AppStatus = {
+            UNKNOWN: 'unknown',
+    /** The App has been constructed but that's it. */
+            CONSTRUCTED: 'constructed',
+            /** The App's `initialize()` was called and returned true. */
+            INITIALIZED: 'initialized',
+            /** The App's `onEnable()` was called, returned true, and this was done automatically (system start up). */
+            AUTO_ENABLED: 'auto_enabled',
+            /** The App's `onEnable()` was called, returned true, and this was done by the user such as installing a new one. */
+            MANUALLY_ENABLED: 'manually_enabled',
+            /**
+             * The App was disabled due to an error while attempting to compile it.
+             * An attempt to enable it again will fail, as it needs to be updated.
+             */
+            COMPILER_ERROR_DISABLED: 'compiler_error_disabled',
+            /**
+             * The App was disable due to its license being invalid
+             */
+            INVALID_LICENSE_DISABLED: 'invalid_license_disabled',
+            /** The App was disabled due to an unrecoverable error being thrown. */
+            ERROR_DISABLED: 'error_disabled',
+            /** The App was manually disabled by a user. */
+            MANUALLY_DISABLED: 'manually_disabled',
+            INVALID_SETTINGS_DISABLED: 'invalid_settings_disabled',
+            /** The App was disabled due to other circumstances. */
+            DISABLED: 'disabled',
+        }
+
+        console.log("--------------------------runCode-----------------------");
+        console.log(`[runCode] code`, code)
+        const status = `var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+            function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+            return new (P || (P = Promise))(function (resolve, reject) {
+                function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+                function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+                function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+                step((generator = generator.apply(thisArg, _arguments || [])).next());
+            });
+        };
+        Object.defineProperty(exports, "__esModule", { value: true });\nconst AppStatus_1 = ${JSON.stringify({AppStatus})};\n`;
+        const app2 = status.concat(App.toString());
+        const aaa = app2.concat(code);
+        const a = aaa.replace(
+            'require("@rocket.chat/apps-engine/definition/App")',
+            "{ App }"
+        );
+        // AppStatus
+
+        console.log(`[runCode] a`, a)
+
+        // console.log(`[runCode] aaa`, a)
+        // console.log(`runCode`, code.replace('require("@rocket.chat/apps-engine/definition/App")', '{ App }'))
         const vmOptions = {
             ...AppsEngineVM2Runtime.defaultNodeVMOptions,
-            timeout: options?.timeout,
+            // timeout: options?.timeout,
             sandbox: {
                 ...AppsEngineVM2Runtime.defaultNodeVMOptions.sandbox,
-                ...sandbox || {},
-                exports: {},
+                ...(sandbox || {}),
+                exports: true,
+                require: true,
+            },
+            require: {
+                external: true,
+                builtin: ["*"],
+                // import: ['@*'],
+                root: './',
             },
         };
 
-        if (sandbox?.require instanceof Function) {
-            vmOptions.require = {
-                external: ['@rocket.chat/apps-engine'],
-                // resolve: (moduleName, p) => (console.log(`Resolving ${moduleName} from ${p}`), moduleName),
-                customRequire: (...args) => (console.log(`Custom require called with ${args}`), sandbox.require(...args)),
-                context: 'host',
-            };
+        // console.log(`[runCode] vmOptions`, vmOptions);
 
-            delete sandbox.require;
-        }
+        // vmOptions.require = {
+        //     external: true,
+        //     builtin: ['*'],
+        //     // import: ['@rocket.chat/apps-engine'],
+        // import: ['@rocket.chat/apps-engine'],
+        //     // resolve: (moduleName, p) => (console.log(`Resolving ${moduleName} from ${p}`), moduleName),
+        //     // customRequire: (...args) => (console.log(`Custom require called with ${args}`), sandbox.require(...args)),
+        // };
 
-        return (new NodeVM(vmOptions)).run(code);
+        // tslint:disable-next-line: max-line-length
+        // console.log(`[runCode] ran`, (new NodeVM(vmOptions)).run(code.replace('require("@rocket.chat/apps-engine/definition/App")', '{ App }'), 'node_modules'));
+
+        return new NodeVM(vmOptions).run(a)
     }
 
     private vm: NodeVM;
 
-    constructor(private readonly app: App, customRequire: (mod: string) => any) {
+    constructor(
+        private readonly app: App,
+        customRequire: (mod: string) => any
+    ) {
         super(app, customRequire);
 
         this.vm = new NodeVM({
@@ -54,21 +128,29 @@ export class AppsEngineVM2Runtime extends AppsEngineRuntime {
         });
     }
 
-    public async runInSandbox(code: string, sandbox?: Record<string, any>, options?: IAppsEngineRuntimeOptions): Promise<any> {
+    public async runInSandbox(
+        code: string,
+        sandbox?: Record<string, any>,
+        options?: IAppsEngineRuntimeOptions
+    ): Promise<any> {
         sandbox ??= {};
 
         this.vm.setGlobals(sandbox);
 
         const result = await this.vm.run(code, {
-            filename: getFilenameForApp(options?.filename || this.app.getName()),
+            filename: getFilenameForApp(
+                options?.filename || this.app.getName()
+            ),
         });
 
         // Clean up the sandbox after the code has run
-        this.vm.setGlobals(Object.keys(sandbox).reduce((acc, key) => {
-            acc[key] = undefined;
+        this.vm.setGlobals(
+            Object.keys(sandbox).reduce((acc, key) => {
+                acc[key] = undefined;
 
-            return acc;
-        }, {} as typeof sandbox));
+                return acc;
+            }, {} as typeof sandbox)
+        );
 
         return result;
     }
