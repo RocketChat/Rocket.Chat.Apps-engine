e316b6e6843c53364d45ab48ca083d3414e51479
thassiov <tvmcarvalho@gmail.com>
2022-08-25 09:35:25 -0300

    Adjust types required by the main repo

--- src/server/bridges/InternalBridge.ts
+++ src/server/bridges/InternalBridge.ts
@@ -2,7 +2,7 @@ import { ISetting } from '../../definition/settings';
 import { BaseBridge } from './BaseBridge';
 
 export abstract class InternalBridge extends BaseBridge {
-   public async doGetUsernamesOfRoomById(roomId: string): Promise<Array<string>> {
+   public doGetUsernamesOfRoomById(roomId: string): Array<string> {
        return this.getUsernamesOfRoomById(roomId);
     }
 
@@ -10,6 +10,6 @@ export abstract class InternalBridge extends BaseBridge {
       return this.getWorkspacePublicKey();
     }
 
-   protected abstract getUsernamesOfRoomById(roomId: string): Promise<Array<string>>;
+   protected abstract getUsernamesOfRoomById(roomId: string): Array<string>;
    protected abstract getWorkspacePublicKey(): Promise<ISetting>;
 }
