838ad178c30109feca5ebf2e39494f6cb7fe8a15
Debdut Chakraborty <debdut.chakraborty@rocket.chat>
2022-11-16 03:31:30 +0530

    New: capability of muting or unmuting a user in a room

---
+++ src/server/accessors/RoomUpdater.ts
@@ -0,0 +1,17 @@
+import { IRoomUpdater } from '../../definition/accessors/IRoomUpdater';
+import { IRoom } from '../../definition/rooms';
+import { IUser } from '../../definition/users';
+import { AppBridges } from '../bridges';
+
+export class RoomUpdater implements IRoomUpdater {
+    constructor(private readonly bridges: AppBridges, private readonly appId: string) {
+    }
+
+    public async muteUser(room: IRoom, executor: IUser, user: IUser) {
+        return this.bridges.getRoomBridge().doMuteUser(room.id, executor.id, user.id, this.appId);
+    }
+
+    public async unmuteUser(room: IRoom, executor: IUser, user: IUser) {
+        return this.bridges.getRoomBridge().doUnmuteUser(room.id, executor.id, user.id, this.appId);
+    }
+}
