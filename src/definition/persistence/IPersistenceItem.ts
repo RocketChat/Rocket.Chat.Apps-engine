0dfa632a3ec8a22339f730e835fab6dde0a5d05c
Douglas Gubert <douglas.gubert@gmail.com>
2023-01-06 15:25:55 -0300

    Make associations property not optional on persistence item

--- src/definition/persistence/IPersistenceItem.ts
+++ src/definition/persistence/IPersistenceItem.ts
@@ -3,5 +3,5 @@ import { RocketChatAssociationRecord } from '../../definition/metadata';
 export interface IPersistenceItem {
     appId: string;
     data: Record<string, any>;
-    associations?: Array<RocketChatAssociationRecord>;
+    associations: Array<RocketChatAssociationRecord>;
 }
