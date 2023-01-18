4a375506864afa67e84d952576c090a09538a03b
Douglas Gubert <douglas.gubert@gmail.com>
2023-01-18 18:18:49 -0300

    Update src/definition/persistence/IPersistenceItem.ts

--- src/definition/persistence/IPersistenceItem.ts
+++ src/definition/persistence/IPersistenceItem.ts
@@ -3,5 +3,5 @@ import { RocketChatAssociationRecord } from '../../definition/metadata';
 export interface IPersistenceItem {
     appId: string;
     data: Record<string, unknown>;
-    associations: Array<RocketChatAssociationRecord>;
+    associations?: Array<RocketChatAssociationRecord>;
 }
