3e15ce9bfb685a37efd6abfa9ffa10a456ddd2b4
albuquerquefabio <albuquerquefabio@icloud.com>
2023-01-27 13:04:58 -0300

    Bridge to delete user created by the app

--- src/definition/accessors/IModifyDeleter.ts
+++ src/definition/accessors/IModifyDeleter.ts
@@ -1,3 +1,7 @@
+import type { IUser, UserType } from '../users';
+
 export interface IModifyDeleter {
     deleteRoom(roomId: string): Promise<void>;
+
+    deleteBotUser(appId: Exclude<IUser['appId'], undefined>, type: UserType.APP | UserType.BOT): Promise<boolean>;
 }
--- src/server/accessors/ModifyDeleter.ts
+++ src/server/accessors/ModifyDeleter.ts
@@ -1,10 +1,15 @@
+import type { IUser, UserType } from './../../definition/users';
 import { IModifyDeleter } from '../../definition/accessors';
 import { AppBridges } from '../bridges';
 
 export class ModifyDeleter implements IModifyDeleter {
-    constructor(private readonly bridges: AppBridges, private readonly appId: string) { }
+    constructor(private readonly bridges: AppBridges, private readonly appId: string) {}
 
     public async deleteRoom(roomId: string): Promise<void> {
         return this.bridges.getRoomBridge().doDelete(roomId, this.appId);
+    }
+
+    public async deleteBotUser(appId: Exclude<IUser['appId'], undefined>, type: UserType.BOT): Promise<boolean> {
+        return this.bridges.getUserBridge().doDeleteUsersCreatedByApp(appId, type);
     }
 }
