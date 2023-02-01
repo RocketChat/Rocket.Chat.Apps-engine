6a2be266c0b34c32c52d098b20eb95456e3932fc
Douglas Gubert <douglas.gubert@gmail.com>
2023-02-01 16:55:43 -0300

    Update src/definition/settings/SettingType.ts

--- src/definition/settings/SettingType.ts
+++ src/definition/settings/SettingType.ts
@@ -7,5 +7,7 @@ export enum SettingType {
     SELECT = 'select',
     STRING = 'string',
     MULTI_SELECT = 'multiSelect',
+	// Renders an input of type 'password' in the form. IMPORTANT - the value will NOT be encrypted
+	// it will be treated as a password just on the screen
     PASSWORD = 'password',
 }
