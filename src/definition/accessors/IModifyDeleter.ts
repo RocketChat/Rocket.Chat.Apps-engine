03bedd3e6ff469e046b138db68703c04b7428b8e
デヴぁんす <61188295+Dnouv@users.noreply.github.com>
2023-05-05 19:43:51 +0530

    feat: Bridge delete, deactivate moderation features (#617)

--- src/definition/accessors/IModifyDeleter.ts
+++ src/definition/accessors/IModifyDeleter.ts
@@ -1,7 +1,10 @@
+import { IMessage } from '../messages';
 import type { IUser, UserType } from '../users';
 
 export interface IModifyDeleter {
     deleteRoom(roomId: string): Promise<void>;
 
     deleteUsers(appId: Exclude<IUser['appId'], undefined>, userType: UserType.APP | UserType.BOT): Promise<boolean>;
+
+    deleteMessage(message: IMessage, user: IUser): Promise<void>;
 }
