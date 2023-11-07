cbb2a832f455684111e3f65574624a21c4e98f77
Douglas Gubert <douglas.gubert@gmail.com>
2023-11-07 19:44:10 -0300

    Handle accessor incoming request

--- src/server/runtime/AppsEngineDenoRuntime.ts
+++ src/server/runtime/AppsEngineDenoRuntime.ts
@@ -6,11 +6,31 @@ import * as jsonrpc from 'jsonrpc-lite';
 
 import type { AppAccessorManager, AppApiManager } from '../managers';
 import type { AppManager } from '../AppManager';
+import { IEnvironmentRead, IEnvironmentWrite, IRead } from '../../definition/accessors';
 
 export type AppRuntimeParams = {
     appId: string;
     appSource: string;
 };
+
+const ALLOWED_ACCESSOR_METHODS = [
+    'getConfigurationExtend',
+    'getEnvironmentRead',
+    'getEnvironmentWrite',
+    'getConfigurationModify',
+    'getReader',
+    'getPersistence',
+    'getHttp',
+] as Array<
+    keyof Pick<
+        AppAccessorManager,
+        'getConfigurationExtend' | 'getEnvironmentRead' | 'getEnvironmentWrite' | 'getConfigurationModify' | 'getReader' | 'getPersistence' | 'getHttp'
+    >
+>;
+
+function isValidOrigin(accessor: string): accessor is typeof ALLOWED_ACCESSOR_METHODS[number] {
+    return ALLOWED_ACCESSOR_METHODS.includes(accessor as any);
+}
 
 /**
  * Resolves the absolute path of the Deno executable
@@ -131,8 +151,97 @@ export class DenoRuntimeSubprocessController extends EventEmitter {
         this.on('ready', this.onReady.bind(this));
     }
 
+    // Probable should extract this to a separate file
+    private async handleAccessorMessage({ payload: { method, id, params } }: jsonrpc.IParsedObjectRequest): Promise<jsonrpc.SuccessObject> {
+        const accessorMethods = method.substring(9).split(':'); // First 9 characters are always 'accessor:'
+        const managerOrigin = accessorMethods.shift();
+        const tailMethodName = accessorMethods.pop();
+
+        /**
+         * At this point, the accessorMethods array will contain the path to the accessor from the origin (AppAccessorManager)
+         * The accessor is the one that contains the actual method the app wants to call
+         *
+         * Most of the times, it will take one step from origin to accessor
+         * For example, for the call AppAccessorManager.getEnvironmentRead().getServerSettings().getValueById() we'll have
+         * the following:
+         *
+         * ```
+         * const managerOrigin = 'getEnvironmentRead'
+         * const tailMethod = 'getValueById'
+         * const accessorMethods = ['getServerSettings']
+         * ```
+         *
+         * But sometimes there can be more steps, like in the following example:
+         * AppAccessorManager.getReader().getEnvironmentReader().getEnvironmentVariables().getValueByName()
+         * In this case, we'll have:
+         *
+         * ```
+         * const managerOrigin = 'getReader'
+         * const tailMethod = 'getValueByName'
+         * const accessorMethods = ['getEnvironmentReader', 'getEnvironmentVariables']
+         * ```
+         **/
+
+        // Prevent app from trying to get properties from the manager that
+        // are not intended for public access
+        if (!isValidOrigin(managerOrigin)) {
+            throw new Error('Invalid accessor namespace');
+        }
+
+        // Need to fix typing of return value
+        const getAccessorForOrigin = (
+            accessorMethods: string[],
+            managerOrigin: typeof ALLOWED_ACCESSOR_METHODS[number],
+            accessorManager: AppAccessorManager,
+        ) => {
+            const origin = accessorManager[managerOrigin](this.appId);
+
+            // These will need special treatment
+            if (managerOrigin === 'getConfigurationExtend' || managerOrigin === 'getConfigurationModify') {
+                return origin[accessorMethods[0] as keyof typeof origin];
+            }
+
+            if (managerOrigin === 'getHttp' || managerOrigin === 'getPersistence') {
+                return origin;
+            }
+
+            let accessor = origin;
+
+            // Call all intermediary objects to "resolve" the accessor
+            accessorMethods.forEach((methodName) => {
+                const method = accessor[methodName as keyof typeof accessor] as unknown;
+
+                if (typeof method !== 'function') {
+                    throw new Error('Invalid accessor method');
+                }
+
+                accessor = method.apply(accessor);
+            });
+
+            return accessor;
+        };
+
+        const accessor = getAccessorForOrigin(accessorMethods, managerOrigin, this.accessors);
+
+        const tailMethod = accessor[tailMethodName as keyof typeof accessor] as unknown;
+
+        if (typeof tailMethod !== 'function') {
+            throw new Error('Invalid accessor method');
+        }
+
+        const result = await tailMethod.apply(accessor, params);
+
+        return jsonrpc.success(id, result);
+    }
+
     private async handleIncomingMessage(message: jsonrpc.IParsedObjectNotification | jsonrpc.IParsedObjectRequest): Promise<void> {
-        const { method, id } = message.payload;
+        const { method } = message.payload;
+
+        if (method.startsWith('accessor:')) {
+            const result = await this.handleAccessorMessage(message as jsonrpc.IParsedObjectRequest);
+
+            this.deno.stdin.write(result.serialize());
+        }
 
         switch (method) {
             case 'ready':
