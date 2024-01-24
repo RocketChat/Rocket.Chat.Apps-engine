3b02c1bc713230041eac0cdcecaf3ba3dc0fc91a
Luis Mauro <mauromb3@gmail.com>
2024-01-23 19:55:17 -0600

    feat: PROSVC-55 findRoomsByAgentId

--- src/server/bridges/LivechatBridge.ts
+++ src/server/bridges/LivechatBridge.ts
@@ -104,6 +104,12 @@ export abstract class LivechatBridge extends BaseBridge {
         }
     }
 
+    public async doFindRoomsByAgentId(agentId: string, appId: string): Promise<Array<ILivechatRoom>> {
+        if (this.hasReadPermission(appId, 'livechat-room')) {
+            return this.findRoomsByAgentId(agentId, appId);
+        }
+    }
+
     public async doFindRooms(visitor: IVisitor, departmentId: string | null, appId: string): Promise<Array<ILivechatRoom>> {
         if (this.hasReadPermission(appId, 'livechat-room')) {
             return this.findRooms(visitor, departmentId, appId);
@@ -170,6 +176,8 @@ export abstract class LivechatBridge extends BaseBridge {
     protected abstract createRoom(visitor: IVisitor, agent: IUser, appId: string, extraParams?: IExtraRoomParams): Promise<ILivechatRoom>;
 
     protected abstract closeRoom(room: ILivechatRoom, comment: string, closer: IUser | undefined, appId: string): Promise<boolean>;
+
+    protected abstract findRoomsByAgentId(agentId: string, appId: string): Promise<Array<ILivechatRoom>>;
 
     protected abstract findRooms(visitor: IVisitor, departmentId: string | null, appId: string): Promise<Array<ILivechatRoom>>;
 
