7362e7eeda9b8f0d90950af81962ad63efe19e07
Douglas Gubert <douglas.gubert@gmail.com>
2024-05-24 15:00:15 -0300

    Fix lint

--- src/definition/messages/IMessageRaw.ts
+++ src/definition/messages/IMessageRaw.ts
@@ -9,7 +9,7 @@ import type { IMessageReactions } from './IMessageReaction';
 /**
  * The raw version of a message, without resolved information for relationship fields, i.e.
  * `room`, `sender` and `editor` are not the complete entity like they are in `IMessage`
- * 
+ *
  * This is used in methods that fetch multiple messages at the same time, as resolving the relationship
  * fields require additional queries to the database and would hit the system's performance significantly.
  */
