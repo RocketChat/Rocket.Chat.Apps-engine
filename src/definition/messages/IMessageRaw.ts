cb7775b3ff5e200c30dce81104e8255f82699e12
Douglas Gubert <douglas.gubert@gmail.com>
2024-05-24 14:54:05 -0300

    Add comment to IMessageRaw

--- src/definition/messages/IMessageRaw.ts
+++ src/definition/messages/IMessageRaw.ts
@@ -6,6 +6,13 @@ import type { IMessageAttachment } from './IMessageAttachment';
 import type { IMessageFile } from './IMessageFile';
 import type { IMessageReactions } from './IMessageReaction';
 
+/**
+ * The raw version of a message, without resolved information for relationship fields, i.e.
+ * `room`, `sender` and `editor` are not the complete entity like they are in `IMessage`
+ * 
+ * This is used in methods that fetch multiple messages at the same time, as resolving the relationship
+ * fields require additional queries to the database and would hit the system's performance significantly.
+ */
 export interface IMessageRaw {
     id?: string;
     threadId?: string;
