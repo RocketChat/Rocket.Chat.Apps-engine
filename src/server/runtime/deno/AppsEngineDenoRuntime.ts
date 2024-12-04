39609338b94a3138004e10acc2b9f0ddd538e102
Douglas Gubert <douglas.gubert@gmail.com>
2024-12-03 22:51:29 -0300

    make Deno capture unhandled exceptions and rejections and report them to the server
    https://github.com/RocketChat/Rocket.Chat/pull/33997

--- src/server/runtime/deno/AppsEngineDenoRuntime.ts
+++ src/server/runtime/deno/AppsEngineDenoRuntime.ts
@@ -13,6 +13,7 @@ import type { IParseAppPackageResult } from '../../compiler';
 import { AppConsole, type ILoggerStorageEntry } from '../../logging';
 import type { AppAccessorManager, AppApiManager } from '../../managers';
 import { AppStatus } from '../../../definition/AppStatus';
+import type { AppMethod } from '../../../definition/metadata';
 import { bundleLegacyApp } from './bundler';
 import { ProcessMessenger } from './ProcessMessenger';
 import { LivenessManager } from './LivenessManager';
@@ -535,10 +536,27 @@ export class DenoRuntimeSubprocessController extends EventEmitter {
             case 'log':
                 console.log('SUBPROCESS LOG', message);
                 break;
+            case 'unhandledRejection':
+            case 'uncaughtException':
+                await this.logUnhandledError(`runtime:${method}`, message);
+                break;
+
             default:
                 console.warn('Unrecognized method from sub process');
                 break;
         }
+    }
+
+    private async logUnhandledError(
+        method: `${AppMethod.RUNTIME_UNCAUGHT_EXCEPTION | AppMethod.RUNTIME_UNHANDLED_REJECTION}`,
+        message: jsonrpc.IParsedObjectRequest | jsonrpc.IParsedObjectNotification,
+    ) {
+        this.debug('Unhandled error of type "%s" caught in subprocess', method);
+
+        const logger = new AppConsole(method);
+        logger.error(message.payload);
+
+        await this.logStorage.storeEntries(AppConsole.toStorageEntry(this.getAppId(), logger));
     }
 
     private async handleResultMessage(message: jsonrpc.IParsedObjectError | jsonrpc.IParsedObjectSuccess): Promise<void> {
