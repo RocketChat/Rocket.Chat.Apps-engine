19d74584d7bca833a87a8e063848b0235f7d25f9
Douglas Gubert <douglas.gubert@gmail.com>
2025-01-06 21:05:59 -0300

    fix: interval for metrics kept subprocess alive
    https://github.com/RocketChat/Rocket.Chat/pull/34894

--- deno-runtime/lib/metricsCollector.ts
+++ deno-runtime/lib/metricsCollector.ts
@@ -1,5 +1,5 @@
-import { writeAll } from "https://deno.land/std@0.216.0/io/write_all.ts";
-import { Queue } from "./messenger.ts";
+import { writeAll } from 'https://deno.land/std@0.216.0/io/write_all.ts';
+import { Queue } from './messenger.ts';
 
 export function collectMetrics() {
     return {
@@ -14,18 +14,4 @@ export async function sendMetrics() {
     const metrics = collectMetrics();
 
     await writeAll(Deno.stderr, encoder.encode(JSON.stringify(metrics)));
-}
-
-let intervalId: number;
-
-export function startMetricsReport(frequencyInMs = 5000) {
-    if (intervalId) {
-        throw new Error('There is already an active metrics report');
-    }
-
-    intervalId = setInterval(sendMetrics, frequencyInMs);
-}
-
-export function abortMetricsReport() {
-    clearInterval(intervalId);
 }
